de40194dadf7c281761a78177368cadd18bb7e7e
Vinicius <viniciusgarciadosreis@gmail.com>
2020-09-19 14:36:40 -0300

    Configurando front-web para producao

--- front-web/src/pages/Records/index.tsx
+++ front-web/src/pages/Records/index.tsx
@@ -7,7 +7,7 @@ import Pagination from './Pagination';
 import Filters from '../../components/Filters';
 
 
-const BASE_URL = 'http://localhost:8081'
+const BASE_URL = 'https://sds1-vinicius.herokuapp.com';
 
 const Records = () => {
     const [ recordsResponse, setRecordsResponse] = useState<RecordsResponse>();
